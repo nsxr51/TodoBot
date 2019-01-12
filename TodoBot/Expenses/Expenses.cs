8d8c82f2956fa92d512839c7c36dc7b32364c74c
lior bennaim <bennaim.lior@gmail.com>
2019-01-12 23:44:27 +0200

    firebasesupport

--- TodoBot/Expenses/Expenses.cs
+++ TodoBot/Expenses/Expenses.cs
@@ -1,22 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-
+using TodoBot.Expenses;
+using Firebase.Database;
+using Firebase.Database.Query;
 namespace Telegram.Bot.Examples.Echo
 {
-    public enum ExpenseDirection
-    {
-        OutCome,
-        InCome
-    }
 
-    public enum expanseType
-    {
-        CreditCard,
-        Cash
-    }
-    public class Expenses
+    public class Expenses: IExpenses
     {
         private static Expenses _instance;
         public static Expenses Instance
@@ -57,6 +48,7 @@ namespace Telegram.Bot.Examples.Echo
                 string[] expen = line.Split(',');
                 AddNewExpense(new Expense(expen[0], expen[1], expen[2], expen[4], expen[3]));
             }
+
         }
         public static string[] GetExpensType()
         {
@@ -82,53 +74,11 @@ namespace Telegram.Bot.Examples.Echo
             }
             return strToret;
         }
-    }
-    public class Expense
-    {
-        public DateTime Date;
-        public double Amount;
-        public ExpenseDirection Direction;
-        public string Subject;
-        public expanseType Type;
-        public Expense(ExpenseDirection dir, expanseType type, string subject)
-        {
-            Date = DateTime.Now;
-            Direction = dir;
-            Subject = subject;
-            Type = type;
-        }
-        public Expense(string date,string dir, string type,string amount, string subject)
-        {
-            Date = DateTime.Parse(date);
-            Direction = dir == "הוצאה" ? ExpenseDirection.OutCome: ExpenseDirection.InCome;
-            Subject = subject;
-            Amount = double.Parse(amount);
-            Type = type == "Cash" ? expanseType.Cash : expanseType.CreditCard;
-        }
-        public void SetAmout(double amount)
-        {
-            amount = Math.Abs(amount);
-            if (Direction == ExpenseDirection.InCome)
-            {
-                Amount = amount;
-            }
-            else
-            {
-                Amount = amount * -1;
-            }
-        }
 
-        public byte[] ToByteArray()
+        void IExpenses.LoadExpenses()
         {
-            string dir = Direction == ExpenseDirection.OutCome ? "הוצאה" : "הכנסה";
-            string str = $"{Date},{dir},{Type.ToString()},{Subject},{Amount}\r\n";
-            return Encoding.UTF8.GetBytes(str);
-        }
-
-        public override string ToString()
-        {
-            string dir = Direction == ExpenseDirection.OutCome ? "הוצאה" : "הכנסה";
-            return $"{Date},{dir},{Type.ToString()},{Subject},{Amount}";
+            throw new NotImplementedException();
         }
     }
+ 
 }
